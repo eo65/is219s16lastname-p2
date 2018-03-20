515775d6fa69508b0ecb93dcec7f818e54a0c421
eo65 <35512966+eo65@users.noreply.github.com>
2018-03-20 18:04:40 -0400

    Completion of Part 1 and Part 2(sections 1-3 except 4)

--- js/gallery.js
+++ js/gallery.js
@@ -34,6 +34,7 @@ function animate() {
 
 function swapPhoto() {
 	//Add code here to access the #slideShow element.
+	
 	//Access the img element and replace its source
 	//with a new image from your images array which is loaded 
 	//from the JSON string
@@ -43,18 +44,21 @@ function swapPhoto() {
 // Counter for the mImages array
 var mCurrentIndex = 0;
 
+
 // XMLHttpRequest variable
 var mRequest = new XMLHttpRequest();
 
 // Array holding GalleryImage objects (see below).
 var mImages = [];
 
+
 // Holds the retrived JSON information
 var mJson;
 
+
 // URL for the JSON to load by default
 // Some options for you are: images.json, images.short.json; you will need to create your own extra.json later
-var mUrl = 'insert_url_here_to_image_json';
+var mUrl;
 
 
 //You can optionally use the following function as your event callback for loading the source of Images from your json data (for HTMLImageObject).
@@ -79,10 +83,46 @@ window.addEventListener('load', function() {
 
 }, false);
 
-function GalleryImage() {
+
+mImages.push(new GalleryImage("../img/places/australia.jpg", "Australia", "Loch Ard Gorge", "01/01/2016" ));
+mImages.push(new GalleryImage("../img/places/austria.jpg", "Austria", "Austrian chapel", "01/02/2016"));
+mImages.push(new GalleryImage("../img/places/france.jpg", "Paris", "Eiffel Tower", "01/03/2016"));
+mImages.push(new GalleryImage("../img/places/greece.jpg", "Greece", "Greek coastline", "01/04/2016"));
+
+
+function GalleryImage(plc, des, dat, img) {
 	//implement me as an object to hold the following data about an image:
-	//1. location where photo was taken
-	//2. description of photo
-	//3. the date when the photo was taken
-	//4. either a String (src URL) or an an HTMLImageObject (bitmap of the photo. https://developer.mozilla.org/en-US/docs/Web/API/HTMLImageElement)
+	
+	this.img = image;			//1. either a String (src URL) or an an HTMLImageObject (bitmap of the photo. https://developer.mozilla.org/en-US/docs/Web/API/HTMLImageElement)
+	this.plc = place;			//2. location where photo was taken
+	this.des = description;		//3. description of photo
+	this.dat = date; 			//4. the date when the photo was taken
 }
+
+
+mUrl = "../images.json";
+var mRequest = new XMLHttpRequest();
+mRequest.onreadystatechange = function() {
+	
+	if (mRequest.readyState == 4 && mRequest.status == 200) {
+		try {
+			// Let’s try and see if we can parse JSON (see next slide)
+	
+			mJson = JSON.parse(mRequest.responseText);
+			// LOOP THROUGH the mJSON array here and fill up the
+			// mImages array with GalleryImage objects
+			// Let’s print out the JSON; It will likely show as “obj”
+			
+			for( var i = 0; mJson.images.length; i++) {
+				mImages.push(new GalleryImage((mJson.images[i].image, mJson.images[i].place, mJson.images[i].description, mJson.images[i].date)));
+				console.log(mJson);
+			} catch(err) {
+				console.log(err.message)
+			}
+		}
+	}
+	else alert("ERROR ACCESS JSON FILE!!!!!")
+};
+
+mRequest.open("GET",mUrl, true);
+mRequest.send();
